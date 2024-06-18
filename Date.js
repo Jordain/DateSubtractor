65a0b25dad42b084f08a87504ccaba0d09706884
Jordain <jordainjoyce@hotmail.com>
2024-06-18 12:22:31 -0400

    second commit master

--- Date.js
+++ Date.js
@@ -30,3 +30,5 @@ app.listen(port, () => {
 //So it will keep the commits until I create a new branch otherwise I should delete and reclone it to reduce issues when updating.
 
 //This is in a new branch.
+
+//second commit
